8e02c59ec82adf17007f83173a829694006533e0
Kyle <plusultra0322@gmail.com>
2022-12-08 20:15:45 +0800

    ADD-made a html  when schedule import in our page

--- main.js
+++ main.js
@@ -62,16 +62,43 @@ app.post("/schedule",encoder,function(req,res){
     let date = req.body.date;
     let dateSplit = date.split('-');
     taiwanYear = parseInt(dateSplit[0])-1911;
-    let taiwanDate = taiwanYear.toString() + dateSplit[1] + dateSplit[2];
+    global.taiwanDate;
+    taiwanDate = taiwanYear.toString() + dateSplit[1] + dateSplit[2];
     
-    connection.query(`select 房號 from 手術資料 where 手術日期=${taiwanDate} and 房號 != ''`, (err, result) => {
-        if(err) {
-            console.log(err);
-        } else {
-            console.log(result);
-        }
-    });
+    res.redirect('/schedule/operation');
     res.end();
+});
+
+app.get("/schedule/operation",function(req,res){
+    var room = [];
+
+    connection.query('select count(房號) from 手術資料 where 房號 != ""', (err, result) => {
+
+        let len = result[0]['count(房號)'];
+        
+        connection.query(`select 房號 from 手術資料 where 房號 != '' order by 房號`, (err, result) => {
+            if(err) {
+                console.log(err);
+            } else {
+                for(let i =0; i < len; i++) {
+                    roomNumber = room.includes(result[i]['房號']);
+                    if(roomNumber == false) {
+                        room.push(result[i]['房號']);
+                    }
+                }
+            };
+           room.pop(); 
+           connection.query(`select 名字 from 使用者帳號 where ID = ${global.ID}`, (err, result) => {
+                username = result[0]['名字'];
+                connection.query(`select * from 手術資料 where 手術日期 = ${taiwanDate}`, (err, result) => {
+                    operationData = result;
+                    console.log(operationData);
+                    res.render('has_operation_schedule_page', {room: room, username: username, operationData: operationData});
+                });
+               
+           })
+        });
+    });    
 });
 
 app.listen(3000);
